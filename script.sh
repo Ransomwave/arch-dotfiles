179283d6a34e357cf3be8062499a0ec8fa36d134
Ransomwave <63156992+Ransomwave@users.noreply.github.com>
2026-05-26 20:33:31 +0000

    fix: add sudo to package management commands and improve script structure

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+set -e
 echo Please make sure your system is already set up with a user account and that you have credentials for sudo!
 read -p "Press enter to continue..."
 
 echo Updating the pacman registry...
-pacman -Syu
+sudo pacman -Syu
 
 echo Installing the micro editor...
-pacman -S micro --noconfirm
+sudo pacman -S micro --noconfirm
 
 echo Installing X, i3 and related packages...
 sudo pacman -S \
@@ -18,20 +20,44 @@ alacritty \
 picom \
 feh \
 dunst \
-network-manager-applet
+networkmanager \
+network-manager-applet \ 
+--noconfirm
 
 echo Downloading wallpaper...
 mkdir -p ~/Pictures/wallpapers
-curl https://github.com/MrDLingters/MonochromeWin11/blob/main/Wallpapers/wallhaven-l36zrl.jpg > ~/Pictures/wallpapers/wallpaper.jpg
+sudo pacman -S curl --noconfirm
+curl -L https://raw.githubusercontent.com/MrDLingters/MonochromeWin11/main/Wallpapers/wallhaven-l36zrl.jpg -o ~/Pictures/wallpaper.jpg
 
 echo Setting up i3...
 echo "exec i3" > ~/.xinitrc
-# (we should copy the configs from the repo)
+mkdir -p ~/.config/i3
+cp ./i3/config ~/.config/i3/config
 
-echo Setting up Polybar
-# (copy configs from repo)
+echo Setting up Polybar...
+mkdir -p ~/.config/polybar
+cp -r ./polybar/* ~/.config/polybar/
 chmod +x ~/.config/polybar/launch.sh
 
-echo Starting NetworkManager
+echo Setting up Picom...
+mkdir -p ~/.config/picom
+cp ./picom/picom.conf ~/.config/picom/picom.conf
+
+echo Setting up Alacritty...
+mkdir -p ~/.config/alacritty
+cp -r ./alacritty/* ~/.config/alacritty/
+
+echo Setting up Rofi...
+mkdir -p ~/.config/rofi
+cp ./rofi/config.rasi ~/.config/rofi/config.rasi
+
+echo Copying gtk theme...
+mkdir -p ~/.config/gtk-3.0
+cp ./gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+
+echo Starting NetworkManager..
 systemctl enable NetworkManager
 systemctl start NetworkManager
+
+echo Finished!
+echo Get into the desktop environment by running "startx".
